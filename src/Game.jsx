787022ab51ccc97290360d53ef1392c671bf784c
monesh <goyalmonesh2201@gmail.com>
2021-07-10 21:46:31 +0530

    changed rules

--- src/Game.jsx
+++ src/Game.jsx
@@ -40,7 +40,6 @@ const Game = () => {
       <div>
 					<h1>Rule:</h1>
 					<h1>The objective is to collect the most pairs of cards.</h1>
-					<h1>When the game starts, Shuffle the cards and lay them on the table, face down, in rows.</h1>
 					<h1>On each turn, a player turns over any two cards (one at a time) and keeps them and gain one point if the cards match</h1>
 					<h1>When a player turns over two cards that do not match, the game will be over.</h1>
 					<h1>The trick is to remember which cards are where.</h1>
