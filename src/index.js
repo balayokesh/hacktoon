a0c81975d02a85e5b7bdaf3109b50f52b9b397c3
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-10 11:54:10 +0530

    rectify file import error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import "./index.css";
 
 // Import components
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
