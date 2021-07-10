ef0a6c7660893bdb52fa7c1a8709b3a62aa619eb
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-10 13:51:36 +0530

    changes for calling reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, { useState} from 'react';
 
+// Import Material-UI components
+import { CssBaseline, Container, Card, CardMedia, CardContent, CardActions, Button, Typography, Grid } from '@material-ui/core';
+
+// Import assets
+import logo from './assets/images/logo.png'
+
+// Import custom components
+import CardComponent from './Components/CardComponent.jsx';
+
 const App = () => {
     return (
-
         <>
+			<CssBaseline />
 			<header className="header">
+				<img src={logo} alt='mickey logo' />
 				<h3 className="logo">Mickey-Hacktoon</h3>
 				<div className="participants">
 					<h2>Monesh Goyal</h2>
@@ -14,12 +24,30 @@ const App = () => {
 			</header>
 
 			<main className="main_div">
-				<h1>Rule:</h1>
-				<h1>The objective is to collect the most pairs of cards.</h1>
-				<h1>When the game starts, Shuffle the cards and lay them on the table, face down, in rows.</h1>
-				<h1>On each turn, a player turns over any two cards (one at a time) and keeps them and gain one point if the cards match</h1>
-				<h1>When a player turns over two cards that do not match, the game will be over.</h1>
-				<h1>The trick is to remember which cards are where.</h1>
+				<div>
+					<h1>Rule:</h1>
+					<h1>The objective is to collect the most pairs of cards.</h1>
+					<h1>When the game starts, Shuffle the cards and lay them on the table, face down, in rows.</h1>
+					<h1>On each turn, a player turns over any two cards (one at a time) and keeps them and gain one point if the cards match</h1>
+					<h1>When a player turns over two cards that do not match, the game will be over.</h1>
+					<h1>The trick is to remember which cards are where.</h1>
+				</div>
+				<div>
+					<Container maxWidth='md'>
+						<Grid container>
+							<Grid item>
+								<CardComponent 
+									image='https://source.unsplash.com/random'
+									title='test title'
+								/>
+								<CardComponent 
+									image='https://source.unsplash.com/random'
+									title='test title'
+								/>
+							</Grid>	
+						</Grid>	
+					</Container>
+				</div>
 			</main>
 
 			<footer className="footer">
@@ -27,7 +55,6 @@ const App = () => {
 			</footer>
 
         </>
-
     )
 }
 
