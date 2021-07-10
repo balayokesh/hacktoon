47259a7718cfb62998899bd1ca2f4b957b16f8dd
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-10 16:09:42 +0530

    implement router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+// Import npm packages
 import React, { useState} from 'react';
+import { Link } from 'react-router-dom';
 
 // Import Material-UI components
-import { AppBar, Toolbar, CssBaseline, Container, Grid, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, CssBaseline, Container, Grid, Typography} from '@material-ui/core';
 import useStyles from './assets/styles/style';
 
 // Import assets
@@ -9,7 +11,6 @@ import logo from './assets/images/logo.png'
 
 // Import custom components
 import CardComponent from './Components/CardComponent.jsx';
-import Cards from './Cards.jsx';
 
 const App = () => {
 	const classes = useStyles();
@@ -49,6 +50,18 @@ const App = () => {
 
 				<div>
 					<Container>
+						<Typography variant='h2' align='center'>
+							<Link to='/game'>
+								Play game
+							</Link>				
+						</Typography>
+					</Container>
+				</div>
+
+				<br />
+
+				<div>
+					<Container>
 						<Typography variant='h2'>
 							About Mickey Mouse:
 						</Typography>
@@ -73,30 +86,11 @@ const App = () => {
 						</Typography>
 					</Container>
 				</div>
-
-				<div>
-					<h1>Rule:</h1>
-					<h1>The objective is to collect the most pairs of cards.</h1>
-					<h1>When the game starts, Shuffle the cards and lay them on the table, face down, in rows.</h1>
-					<h1>On each turn, a player turns over any two cards (one at a time) and keeps them and gain one point if the cards match</h1>
-					<h1>When a player turns over two cards that do not match, the game will be over.</h1>
-					<h1>The trick is to remember which cards are where.</h1>
-					<h1>Rules:</h1>
-				
-					<h2>The objective is to collect the most pairs of cards.</h2>
-					<h2>When the game starts, Shuffle the cards and lay them on the table, face down, in rows.</h2>
-					<h2>On each turn, a player turns over any two cards (one at a time) and keeps them and gain one point if the cards match</h2>
-					<h2>When a player turns over two cards that do not match, the game will be over.</h2>
-					<h2>The trick is to remember which cards are where.</h2>
-				</div>
-
-				<Cards />
 			</main>
 
 			<footer className="footer">
 				{/* To be filled later */}
 			</footer>
-
         </>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+// Import npm packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 // Import components
 import App from './App.jsx';
+import Game from './Game.jsx'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routing = () => {
+    return (
+      <BrowserRouter>
+        <Route path="/" exact component={App} />
+        <Route path="/game" component={Game} />
+      </BrowserRouter>
+    );
+  }
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
